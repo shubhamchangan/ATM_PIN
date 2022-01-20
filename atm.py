3c1658e8b73720db7bd857eee0464c7e5267f1c4
shubhamchangan <98081243+shubhamchangan@users.noreply.github.com>
2022-01-20 18:42:38 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,29 @@
+# atm_pin
+import time
+nm = input('Enter your name:')
+print('Welcome',nm,'to SBI ATM')
+print('Insert the card')
+time.sleep(4)
+print('Its processing...')
+time.sleep(3)
+print('Sucessfull....')
+pin = input('Enter your Pin:')
+time.sleep(2)
+print('Select the option:')
+print('1.Mini statement\n2.Banking\n3.Withdraw')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
